5b8f4da3f6fc2647a6ae38af22d553089114f552
Jakub Sokół <rousanter@MacBook-Air-Jakub.local>
2023-01-08 18:00:53 +0100

    work in progress

---
+++ src/chart1.js
@@ -0,0 +1,28 @@
+
+function createChart(dict) {
+    labels = Object.keys(dict)
+    data = []
+    for (var i=0, size=labels.length;i<size;i++){
+        data.push(dict[labels[i]])
+    }
+    var ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'most popular cars',
+                data: data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
---
+++ src/utilities.js
@@ -0,0 +1,34 @@
+function createURL() {
+    return "https://my.api.mockaroo.com/groupProject.json?key=018bd330"
+}
+async function apiRequest() {
+    var response = await fetch(createURL())
+    return response.json()
+}
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+function cars(data){
+    var carsList=[]
+    for (var i=0, size=data.length;i<size;i++) {
+        carsList.push(data[i]["car"])
+    }
+    carsList= carsList.filter(onlyUnique)
+
+    var carsDict={}
+    for (var i=0, size=carsList.length;i<size;i++) {
+        carsDict[carsList[i]]=0
+    }
+
+    for (var i=0, size=data.length;i<size;i++) {
+        carsDict[data[i]["car"]] += 1
+    }
+
+    return carsDict
+}
+
+apiRequest()
+    .then(data => cars(data))
+    .then(dict => createChart(dict))
